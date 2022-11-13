54cbb092650d2d6323dbcebab5d64868a7a0674d
BkMo <777373+bkmo@users.noreply.github.com>
2022-11-13 14:11:00 -0600

    arkenstuff

--- arkenstuff/user-overrides.js
+++ arkenstuff/user-overrides.js
@@ -4,7 +4,6 @@ user_pref("privacy.resistFingerprinting", false); // 4501
 user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504 [pointless if not using RFP]
 user_pref("webgl.disabled", false); // 4520 [mostly pointless if not using RFP]
 user_pref("privacy.clearOnShutdown.history", false); // 2811
-user_pref("network.http.referer.XOriginPolicy", 2; //fix bank sites cross referer 1601
 user_pref("keyword.enabled", true);
 user_pref("browser.startup.page", 1);
 user_pref("browser.startup.homepage", "https://news.google.com/topstories?hl=en-US&gl=US&ceid=US:en|https://www.apriliaforum.com/forums/forumdisplay.php?11-Aprilia-1000cc-V-Twins-Model-Years-1998-2009");
