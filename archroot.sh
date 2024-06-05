b67f4f9c06f8b684b66808f7f762d2da10c94c48
Brian Morison <777373+bkmo@users.noreply.github.com>
2024-06-05 01:48:06 -0600

    Update archroot.sh

--- archroot.sh
+++ archroot.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
-cryptsetup luksOpen /dev/nvme0n1p2 cryptroot
+setfont -d
+echo
+echo "Decrypting and Mounting"
+echo
+cryptsetup luksOpen /dev/nvme0n1p7 cryptroot
 cryptsetup luksOpen /dev/nvme0n1p3 storage
 
-mount -o noatime,compress=zstd,subvol=@ /dev/mapper/cryptroot /mnt
-mount -o noatime,compress=zstd,subvol=@home /dev/mapper/cryptroot /mnt/home
-mount /dev/nvme0n1p1 /mnt/boot
+mkdir /mnt/home
+mkdir /mnt/boot
+mkdir /mnt/.snapshots
+mkdir /tmp/btrfsroot
 
-mkdir /mnt/btrfs
-mkdir /mnt/btrfs1
+mount -o rw,noatime,compress=zstd,subvol=@ /dev/mapper/cryptroot /mnt
+mount -o rw,noatime,compress=zstd,subvol=@home /dev/mapper/cryptroot /mnt/home
+mount -o rw,noatime,fmask=0022,dmask=0022 /dev/nvme0n1p2 /mnt/boot
+mount -o rw,noatime,fmask=0022,dmask=0022 /dev/nvme0n1p2 /boot
+mount -o subvolid=5 /dev/mapper/cryptroot /tmp/btrfsroot
+mount -o rw,noatime,compress=zstd,subvol=@snapshots /dev/mapper/cryptroot /mnt/.snapshots
 
-mount -o noatime,compress=zstd /dev/mapper/storage /mnt/btrfs
-mount -o noatime,compress=zstd /dev/mapper/cryptroot /mnt/btrfs1
-
-echo "Sleeping for five seconds, ctrl-c to exit before arch-chroot"
-sleep 5
-arch-chroot /mnt
+echo
+echo  "Mounted and ready to arch-chroot or Snapper-Rollback"
+echo
+echo  "Commands to setup wifi connection:"
+echo  "iwctl"
+echo  "station wlan0 connect Casita5"
+echo  "exit from iwd-iwctl"
+echo  "ping -c 3 google.com"
+echo
+echo  "arch-chroot /mnt to chroot"
+echo  "btrfs subvol list /tmp/btrfsroot  snapper-rollback num"
+echo
+echo
