56cae9c640f23b1e6deda5d4ed9453c4e3c07af8
Brian Morison <777373+bkmo@users.noreply.github.com>
2025-08-22 17:04:50 -0600

    Update archroot.sh

--- archroot.sh
+++ archroot.sh
@@ -7,15 +7,15 @@ cryptsetup luksOpen /dev/nvme0n1p7 cryptroot
 cryptsetup luksOpen /dev/nvme0n1p3 storage
 
 mkdir /mnt/home
-mkdir /mnt/boot
+mkdir /mnt/efi
 mkdir /mnt/.snapshots
-mkdir /tmp/btrfsroot
+mkdir /mnt/btrfsroot
 
 mount -o rw,noatime,compress=zstd,subvol=@ /dev/mapper/cryptroot /mnt
 mount -o rw,noatime,compress=zstd,subvol=@home /dev/mapper/cryptroot /mnt/home
-mount -o rw,noatime,fmask=0022,dmask=0022 /dev/nvme0n1p2 /mnt/boot
-mount -o rw,noatime,fmask=0022,dmask=0022 /dev/nvme0n1p2 /boot
-mount -o subvolid=5 /dev/mapper/cryptroot /tmp/btrfsroot
+mount -o rw,noatime,fmask=0022,dmask=0022 /dev/nvme0n1p2 /mnt/efi
+mount -o rw,noatime,fmask=0022,dmask=0022 /dev/nvme0n1p2 /efi
+mount -o subvolid=5 /dev/mapper/cryptroot /mnt/btrfsroot
 mount -o rw,noatime,compress=zstd,subvol=@snapshots /dev/mapper/cryptroot /mnt/.snapshots
 
 echo
@@ -28,6 +28,6 @@ echo  "exit from iwd-iwctl"
 echo  "ping -c 3 google.com"
 echo
 echo  "arch-chroot /mnt to chroot"
-echo  "btrfs subvol list /tmp/btrfsroot  snapper-rollback num"
+echo  "btrfs subvol list /mnt/btrfsroot  snapper-rollback num"
 echo
 echo
